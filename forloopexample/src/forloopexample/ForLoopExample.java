eccafd5209a883893ac986beef2791657f20bdf9
GIT12345 <GIT12345@GITSSVK015>
2019-08-10 10:38:47 +0530

    Fourteenth commit

---
+++ forloopexample/src/forloopexample/ForLoopExample.java
@@ -0,0 +1,12 @@
+package forloopexample;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		for(int i=0; i<10; ++i) {
+			System.out.println("The value of i:" +i);
+		}
+			
+	}
+
+}
